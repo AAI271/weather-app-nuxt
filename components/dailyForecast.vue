d2504bcbd9cfe03f46b1455b77927d0e182b8aa4
Abyl239 <altajanovabylay@gmail.com>
2024-10-05 22:12:14 +0600

    forecast days

--- components/dailyForecast.vue
+++ components/dailyForecast.vue
@@ -9,7 +9,7 @@ defineProps<{
 
 <template>
   <div class="w-full flex flex-col gap-4 lg:gap-8 items-center">
-    <h3 class="font-bold text-xl md:text-2xl text-slate-700">10-Day Forecast</h3>
+    <h3 class="font-bold text-xl md:text-2xl text-slate-700">7-Days forecast</h3>
     <div class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4">
       <WeatherCard v-for="day in arrayTransformDailyForecastData(data)" :data="day" />
     </div>
